14555d0ebb7ecb5d4afbf69bd95a00116d568e43
Lucas Guasselli <Lucaszack265@gmail.com>
2019-05-05 03:24:58 -0300

    projeto com thymeleaf executando o html

---
+++ src/main/java/com/lucasguasselli/trabalhoAPIEFramework/TrabalhoApieFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.lucasguasselli.trabalhoAPIEFramework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoApieFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoApieFrameworkApplication.class, args);
+	}
+
+}
